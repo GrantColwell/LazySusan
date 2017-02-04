ff38f9185f342394cb5e9b4824a93f81f45c7cbc
Grant Colwell <Grant Colwell>
2017-02-04 09:37:42 -0500

    second commit

--- src/main/java/ca/_4976/steamworks/subsystems/LazySusan.java
+++ src/main/java/ca/_4976/steamworks/subsystems/LazySusan.java
@@ -9,21 +9,22 @@ import edu.wpi.first.wpilibj.*;
  * Created by Grant on 2/2/2017.
  */
 public class LazySusan extends AsynchronousRobot{
-    int midura = 1;          //midura is a placeholder for midura's ready, searching or failed variable
+    int vision_state = 0;          //midura is a placeholder for midura's ready, searching or failed variable
     public LazySusan(Robot module){
+
         //call midura's code
         module.driver.A.addListener(new ButtonListener() {
             @Override
             public void pressed() {
-                super.pressed();
-                if(midura == 0) {
-                    for(int i = 0; i < 3; i++) {    //buzz's remote 3 times
-                        stick.setRumble(GenericHID.RumbleType.kLeftRumble, 1);
-                        stick.setRumble(GenericHID.RumbleType.kRightRumble, 1);
-                        
+
+                if(vision_state == 0) {
+                    for(int i = 0; i < 2; i++) {    //buzz's remote 3 times
+                        module.driver.setRumble(1);
+
+
                     }
                 }
             }
-        });
+        },500);
     }
 }
